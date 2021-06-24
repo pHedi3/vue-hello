c4e08e1229f512eacf8c265a3f25bf7b6c4fc747
pHedi3 <federico.gualdi3@gmail.com>
2021-06-24 16:18:40 +0200

    fine es

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-      text: 'ciao',
+      text: '',
+      color: '',
+      img: ''
+    },
+    methods: {
+        textRed: function () {
+            this.color = 'red'
+        },
+        textBlu: function () {
+            this.color = 'blue'
+        },
+        textGreen: function () {
+            this.color = 'green'
+        },
+        imgStamp: function() {
+            if (this.img == '') {
+                this.img = 'https://www.opensymbol.it/wp-content/uploads/2021/01/Microsoft-Team-personalizzazione-background.jpg'
+            } else {
+                this.img = ''
+            }
+        }
     }
   })
